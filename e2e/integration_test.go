f41e777fa5cf924907ea9fb62f44e3bc10e7b6ee
Rachit Gupta <rachitgupta1792@gmail.com>
2023-10-04 19:37:20 +0530

    Add login integration tests

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -1,10 +1,15 @@
 package e2e
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/r4chi7/aspire-lite/controller"
 	"github.com/r4chi7/aspire-lite/database"
+	"github.com/r4chi7/aspire-lite/model"
 	"github.com/r4chi7/aspire-lite/repository"
 	"github.com/r4chi7/aspire-lite/service"
 	"github.com/stretchr/testify/suite"
@@ -32,4 +37,34 @@ func (suite *IntegrationTestSuite) SetupSuite() {
 
 func TestIntegrationTestSuite(t *testing.T) {
 	suite.Run(t, new(IntegrationTestSuite))
+}
+
+// Some helper methods
+
+func (suite *IntegrationTestSuite) createUser(email, password string, isAdmin bool) {
+	input := map[string]interface{}{
+		"email":    email,
+		"password": password,
+		"is_admin": isAdmin,
+	}
+	reqBody, _ := json.Marshal(input)
+	req, err := http.NewRequest("POST", "/users", bytes.NewBuffer([]byte(reqBody)))
+	suite.Nil(err)
+
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(suite.userController.Create)
+
+	handler.ServeHTTP(rr, req)
+
+	suite.Equal(http.StatusCreated, rr.Code)
+
+	var resp map[string]interface{}
+	err = json.Unmarshal(rr.Body.Bytes(), &resp)
+	suite.Nil(err)
+	suite.NotEmpty(resp["token"])
+	var insertedUser model.User
+	suite.db.Where("email = ?", "test@example.com").Find(&insertedUser)
+	suite.NotZero(insertedUser.ID)
 }
--- e2e/user_test.go
+++ e2e/user_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/r4chi7/aspire-lite/model"
 )
 
-func (suite *IntegrationTestSuite) TestUser() {
+func (suite *IntegrationTestSuite) TestUserCreate() {
 	req, err := http.NewRequest("POST", "/users", bytes.NewBuffer([]byte(`{"email":"test@example.com","password":"test@123"}`)))
 	suite.Nil(err)
 
@@ -29,4 +29,64 @@ func (suite *IntegrationTestSuite) TestUser() {
 	var insertedUser model.User
 	suite.db.Where("email = ?", "test@example.com").Find(&insertedUser)
 	suite.NotZero(insertedUser.ID)
+}
+
+func (suite *IntegrationTestSuite) TestUserLoginHappyFlow() {
+	suite.createUser("login@example.com", "test@123", false)
+	req, err := http.NewRequest("POST", "/users/login", bytes.NewBuffer([]byte(`{"email":"login@example.com","password":"test@123"}`)))
+	suite.Nil(err)
+
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(suite.userController.Login)
+
+	handler.ServeHTTP(rr, req)
+
+	suite.Equal(http.StatusOK, rr.Code)
+
+	var resp map[string]interface{}
+	err = json.Unmarshal(rr.Body.Bytes(), &resp)
+	suite.Nil(err)
+	suite.NotEmpty(resp["token"])
+}
+
+func (suite *IntegrationTestSuite) TestUserLoginReturnsErrorWhenUserDoesNotExist() {
+	suite.createUser("doesntexist@example.com", "test@123", false)
+	req, err := http.NewRequest("POST", "/users/login", bytes.NewBuffer([]byte(`{"email":"login1@example.com","password":"test@123"}`)))
+	suite.Nil(err)
+
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(suite.userController.Login)
+
+	handler.ServeHTTP(rr, req)
+
+	suite.Equal(http.StatusNotFound, rr.Code)
+
+	var resp map[string]interface{}
+	err = json.Unmarshal(rr.Body.Bytes(), &resp)
+	suite.Nil(err)
+	suite.Equal("user not found", resp["message"])
+}
+
+func (suite *IntegrationTestSuite) TestUserLoginReturnsErrorWhenPasswordDoesNotMatch() {
+	suite.createUser("password@example.com", "test@123", false)
+	req, err := http.NewRequest("POST", "/users/login", bytes.NewBuffer([]byte(`{"email":"password@example.com","password":"test@1235"}`)))
+	suite.Nil(err)
+
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(suite.userController.Login)
+
+	handler.ServeHTTP(rr, req)
+
+	suite.Equal(http.StatusUnauthorized, rr.Code)
+
+	var resp map[string]interface{}
+	err = json.Unmarshal(rr.Body.Bytes(), &resp)
+	suite.Nil(err)
+	suite.Equal("you are unauthorized to perform this action", resp["message"])
 }
